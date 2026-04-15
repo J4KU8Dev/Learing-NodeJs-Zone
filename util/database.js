46154503d5b02ed16bc91ae96c62d57295682c94
Jakub <kubarafalski240703@gmail.com>
2026-04-15 23:48:58 +0200

    adding sql

---
+++ util/database.js
@@ -0,0 +1,10 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'ZAQwsx123!@#'
+});
+
+module.exports = pool.promise()
