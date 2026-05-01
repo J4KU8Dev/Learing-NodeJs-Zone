052822f5bb308b9de68225dd008e29c7a8823ed2
Jakub <kubarafalski240703@gmail.com>
2026-05-01 23:18:07 +0200

    working with cookies, posting orders and cart crashed

--- controllers/admin.js
+++ controllers/admin.js
@@ -7,6 +7,7 @@ exports.getAddProduct = (req, res, next) => {
     pageTitle: "Add Product",
     path: "/admin/add-product",
     editing: false,
+    isAuthenticated: req.session.isLoggedIn,
   });
 };
 
@@ -20,7 +21,7 @@ exports.postAddProduct = (req, res, next) => {
     price: price,
     description: description,
     imageUrl: imageUrl,
-    userId: req.user._id,
+    userId: req.session.user._id,
   });
   product
     .save()
@@ -47,6 +48,7 @@ exports.getEditProduct = (req, res, next) => {
         path: "/admin/edit-product",
         editing: editMode,
         product: product,
+        isAuthenticated: req.session.isLoggedIn,
       });
     })
     .catch((err) => console.log(err));
@@ -84,6 +86,7 @@ exports.getProducts = (req, res, next) => {
         prods: products,
         pageTitle: "Admin Products",
         path: "/admin/products",
+        isAuthenticated: req.session.isLoggedIn,
       });
     })
     .catch((err) => console.error(err));
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,19 @@
+const User = require("../models/user");
+
 exports.getLogin = (req, res, next) => {
   res.render("auth/login", {
     path: "/orders",
     pageTitle: "Login",
+    isAuthenticated: false,
   });
+};
+
+exports.postLogin = (req, res, next) => {
+  User.findById("69f08c41aea8e17eef10e54d")
+    .then((user) => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      res.redirect("/");
+    })
+    .catch((err) => console.log(err));
 };
