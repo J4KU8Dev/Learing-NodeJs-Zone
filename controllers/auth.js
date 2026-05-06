e887a1d17e2a83663a9e48aafa34d100aa8b8e67
Jakub <kubarafalski240703@gmail.com>
2026-05-06 20:28:20 +0200

    adding reseting password function, creating nodemailer

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,12 @@
 const bcrypt = require("bcrypt");
-
+const crypto = require("crypto");
 const User = require("../models/user");
+const nodemailer = require("nodemailer");
+const user = require("../models/user");
+
+const tranporter = nodemailer.createTransport({
+  // configure here sendgrid app
+});
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash("error");
@@ -94,5 +100,54 @@ exports.postLogout = (req, res, next) => {
   req.session.destroy((err) => {
     console.log(err);
     res.redirect("/");
+  });
+};
+
+exports.getReset = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render("auth/reset", {
+    path: "/reset",
+    pageTitle: "Reset",
+    errorMessage: message,
+  });
+};
+
+exports.postReset = (req, res, next) => {
+  crypto.randomBytes(32, (err, buffer) => {
+    if (err) {
+      console.error(err);
+      return res.redirect("/reset");
+    }
+    const token = buffer.toString("hex");
+    User.findOne({ email: req.body.email })
+      .then((user) => {
+        if (!user) {
+          req.flash("error", "Account with such an email doesnt exist");
+          return res.redirect("/reset");
+        }
+        user.userToken = token;
+        user.resetTokenExpiration = Date.now() + 3600000;
+        return user.save();
+      })
+      .then((result) => {
+        res.redirect("/reset");
+        tranporter.sendMail({
+          to: req.body.email,
+          from: "shop@nodeJs.com",
+          subject: "Password Reset",
+          html: `
+          <p>You requested password reset</p>
+          <p>Click this <a href="http://localhost:3000/reset/${token}">link to set a new password</a></p>
+        `,
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   });
 };
