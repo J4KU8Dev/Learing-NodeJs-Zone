7815021bfe8f745317bad8d6a0a376f621f74020
Jakub <kubarafalski240703@gmail.com>
2026-05-21 23:53:16 +0200

    changed deleting function to async, adding payments

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,5 +1,9 @@
 const fs = require("fs");
 const path = require("path");
+const stripe = require("stripe")(
+  "###",
+);
+
 const PDFDocument = require("pdfkit");
 
 const Product = require("../models/product");
@@ -129,6 +133,77 @@ exports.postCartDeleteProduct = (req, res, next) => {
     });
 };
 
+exports.getCheckout = (req, res, next) => {
+  let products;
+  let total = 0;
+  req.user
+    .populate("cart.items.productId")
+    .then((user) => {
+      products = user.cart.items;
+      total = 0;
+      products.forEach((p) => {
+        total += p.quantity * p.productId.price;
+      });
+      return stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items: products.map((p) => {
+          return {
+            name: p.productId.title,
+            description: p.productId.description,
+            amount: p.productId.price * 100,
+            currency: "usd",
+            quantity: p.quantity,
+          };
+        }),
+        success_url: req.protocol + '://' + req.get('host') + '/checkout/successs',
+        cancel_url: req.protocol + '://' + req.get('host') + '/checkout/cancel',
+      });
+    })
+    .then((session) => {
+      res.render("shop/checkout", {
+        path: "/checkout",
+        pageTitle: "Checkout",
+        products: products,
+        totalSum: total,
+        sessionId: session.id,
+      });
+    })
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
+
+exports.getCheckoutSuccess = (req, res, next) => {
+  req.user
+    .populate("cart.items.productId")
+    .then((user) => {
+      const products = user.cart.items.map((i) => {
+        return { quantity: i.quantity, product: { ...i.productId._doc } };
+      });
+      const order = new Order({
+        user: {
+          email: req.user.email,
+          userId: req.user,
+        },
+        products: products,
+      });
+      return order.save();
+    })
+    .then((result) => {
+      return req.user.clearCart();
+    })
+    .then(() => {
+      res.redirect("/orders");
+    })
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
+
 exports.postOrder = (req, res, next) => {
   req.user
     .populate("cart.items.productId")
@@ -157,6 +232,8 @@ exports.postOrder = (req, res, next) => {
       return next(error);
     });
 };
+
+
 
 exports.getOrders = (req, res, next) => {
   Order.find({ "user.userId": req.user._id })
--- public/js/admin.js
+++ public/js/admin.js
@@ -1,4 +1,20 @@
 const deleteProduct = (btn) => {
   const prodId = btn.parentNode.querySelector("[name=productId]").value;
+
+  const productElement = btn.closest('article');
+
+  fetch("/admin/product/" + prodId, {
+    method: "DELETE",
+  })
+    .then((result) => {
+      return result.json();
+    })
+    .then((data) => {
+      console.log(data);
+      productElement.remove();
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 };
 // no added pagination to admin products page
